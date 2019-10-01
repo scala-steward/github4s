e45eb90b4149f148c69bb67aaee958e9097b752c
Scala Steward <me@scala-steward.org>
2019-10-01 14:08:14 +0200

    Update cats-core, cats-free to 2.0.0

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -23,7 +23,7 @@ object ProjectPlugin extends AutoPlugin {
 
     lazy val V = new {
       val base64: String             = "0.2.9"
-      val cats: String               = "1.6.1"
+      val cats: String               = "2.0.0"
       val catsEffect: String         = "1.4.0"
       val circe: String              = "0.11.1"
       val paradise: String           = "2.1.1"
