78668f172b74907b69006fdcaf117801f26cc768
Ben Fradet <benjamin.fradet@gmail.com>
2019-11-28 15:45:34 +0100

    Update circe-core, circe-generic, ... to 0.11.2

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -25,7 +25,7 @@ object ProjectPlugin extends AutoPlugin {
       val base64: String             = "0.2.9"
       val cats: String               = "1.6.1"
       val catsEffect: String         = "2.0.0"
-      val circe: String              = "0.11.1"
+      val circe: String              = "0.11.2"
       val circeJackson: String       = "0.11.1"
       val paradise: String           = "2.1.1"
       val roshttp: String            = "2.2.4"
