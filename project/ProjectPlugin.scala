630be6a96be15b17daace8f9964d6fdd3088be16
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-03-13 12:12:05 +0100

    Update cats-effect to 2.1.1 (#342)

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -23,7 +23,7 @@ object ProjectPlugin extends AutoPlugin {
     lazy val V = new {
       val base64: String     = "0.2.9"
       val cats: String       = "2.1.1"
-      val catsEffect: String = "2.0.0"
+      val catsEffect: String = "2.1.1"
       val circe: String      = "0.13.0"
       val paradise: String   = "2.1.1"
       val simulacrum: String = "0.19.0"
